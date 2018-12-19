b524d076348d49dc1d781b350604b8385b37f852
郑文艳 <zwy@360loyo.com>
2018-12-19 17:40:18 +0800

    first update

---
+++ index.js
@@ -0,0 +1,5 @@
+import Button from './src/components/Button';
+
+module.exports = {
+    Button
+}
---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Button extends Component {
+    render() {
+        return(
+            <div>
+                <button>组件按钮</button>
+            </div>
+        )
+    }
+}
